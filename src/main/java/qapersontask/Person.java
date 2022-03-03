fc5e16bfe87eb64eab5aa67d5ea46152837a4dff
kiran-evans <thekiranevans@gmail.com>
2022-03-03 10:29:14 +0000

    KE: Added pets to each person.

--- src/main/java/qapersontask/Person.java
+++ src/main/java/qapersontask/Person.java
@@ -4,14 +4,15 @@ public class Person {
 	private String name;
 	private int age;
 	private String jobTitle;
+	private Pet pet;
 	
 	public Person() {}
 	
-	public Person(String name, int age, String jobTitle) {
-		super();
+	public Person(String name, int age, String jobTitle, Pet pet) {
 		this.name = name;
 		this.age = age;
 		this.jobTitle = jobTitle;
+		this.pet = pet;
 	}
 	
 	public String getName() {
@@ -35,8 +36,15 @@ public class Person {
 		this.jobTitle = jobTitle;
 	}
 	
+	public Pet getPet() {
+		return pet;
+	}
+	public void setPet(Pet pet) {
+		this.pet = pet;
+	}
+
 	@Override
 	public String toString() {
-		return "Name: " + this.name + ", Age: " + this.age + ", Job Title: " + this.jobTitle;
+		return "\nName: " + this.name + ", Age: " + this.age + ", Job Title: " + this.jobTitle + "\nPet: " + this.pet.toString();
 	}
 }
--- src/main/java/qapersontask/PersonList.java
+++ src/main/java/qapersontask/PersonList.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class PersonList {
 	
-	private Person person1 = new Person("John Smith", 45, "Painter");
-	private Person person2 = new Person("Rita Skeeter", 30, "Journalist");
-	private Person person3 = new Person("Mr Bean", 40, "Adventurer");
+	private Person person1 = new Person("John Smith", 45, "Painter", new Pet("Fluffy", "Dog", 3));
+	private Person person2 = new Person("Rita Skeeter", 30, "Journalist", new Pet("Slimy", "Slug", 1));
+	private Person person3 = new Person("Mr Bean", 40, "Adventurer", new Pet("Gobble", "Turkey", 2));
 	
 	private List<Person> people = Arrays.asList(person1, person2, person3);
 	
